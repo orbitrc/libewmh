d5d623345d8eafcb01f9d791a7e8b3a6ca032424
Yujeonja <hardboiled65@gmail.com>
2021-07-27 08:36:16 +0900

    Application window properties signatures without implementation.

--- include/libewmh/ewmh.h
+++ include/libewmh/ewmh.h
@@ -43,6 +43,40 @@ void ewmh_set_net_active_window(uint32_t window);
 
 char* ewmh_net_wm_name(uint32_t window);
 
+char* ewmh_net_wm_visible_name(uint32_t window);
+
+char* ewmh_net_wm_icon_name(uint32_t window);
+
+char* ewmh_net_wm_visible_icon_name(uint32_t window);
+
+uint32_t ewmh_net_wm_desktop(uint32_t window);
+
+//ewmh_string_list_t ewmh_net_wm_window_type(uint32_t window);
+
+//ewmh_string_list_t ewmh_net_wm_allowed_actions(uint32_t window);
+
+ewmh_uint_list_t ewmh_net_wm_strut(uint32_t window);
+
+ewmh_uint_list_t ewmh_net_wm_strut_partial(uint32_t window);
+
+ewmh_uint_list_t ewmh_net_wm_icon_geometry(uint32_t window);
+
+//void* ewmh_net_wm_icon(uint32_t window);
+
+uint32_t ewmh_net_wm_pid(uint32_t window);
+
+//void* ewmh_net_wm_handled_icons(uint32_t window);
+
+uint32_t ewmh_net_wm_user_time(uint32_t window);
+
+uint32_t ewmh_net_wm_user_time_window(uint32_t window);
+
+ewmh_uint_list_t ewmh_net_wm_frame_extents(uint32_t window);
+
+//void* ewmh_net_wm_opaque_region(uint32_t window);
+
+uint32_t ewmh_net_wm_bypass_compositor(uint32_t window);
+
 EWMH_EXTERN_C_END
 
 #endif /* _LIBEWMH_EWMH_H */
