72eeb642f33dd5197d89e24e31bf5f4d5852dc47
Yujeonja <hardboiled65@gmail.com>
2021-07-27 12:25:32 +0900

    String property generic function.

--- src/ewmh.c
+++ src/ewmh.c
@@ -8,6 +8,8 @@
 
 EWMH_EXTERN_C_BEGIN
 
+char* _ewmh_string_property(uint32_t window, const char *prop);
+
 /*=====================================*/
 /* Root window properties and messages */
 /*=====================================*/
@@ -223,6 +225,59 @@ char* ewmh_net_wm_name(uint32_t window)
 
     xcb_get_property_reply_t *reply = xcb_get_property_reply(
         conn, cookie, NULL);
+    if (reply == NULL) {
+        xcb_disconnect(conn);
+
+        return NULL;
+    }
+
+    val = xcb_get_property_value(reply);
+    if (val != NULL) {
+        val_len = xcb_get_property_value_length(reply);
+        ret = malloc(val_len + 1);
+        memcpy(ret, ((char*)val), (size_t)val_len);
+        ret[val_len] = '\0';
+    }
+
+    /* Free resources */
+    free(reply);
+    xcb_disconnect(conn);
+
+    return ret;
+}
+
+char* ewmh_net_wm_visible_name(uint32_t window)
+{
+    return _ewmh_string_property(window, "_NET_WM_VISIBLE_NAME");
+}
+
+char* ewmh_net_wm_icon_name(uint32_t window)
+{
+    return _ewmh_string_property(window, "_NET_WM_ICON_NAME");
+}
+
+/*===================*/
+/* Generic functions */
+/*===================*/
+
+char* _ewmh_string_property(uint32_t window, const char *prop)
+{
+    xcb_connection_t *conn = xcb_connect(NULL, NULL);
+    void *val = NULL;
+    unsigned int val_len = 0;
+    char *ret = NULL;
+    xcb_atom_t utf8_string = ewmh_get_atom(conn, "UTF8_STRING");
+
+    xcb_get_property_cookie_t cookie = ewmh_get_property_cookie(
+        conn,
+        window,
+        prop,
+        utf8_string
+    );
+
+    xcb_get_property_reply_t *reply = xcb_get_property_reply(
+        conn, cookie, NULL
+    );
     if (reply == NULL) {
         xcb_disconnect(conn);
 
--- tests/main.c
+++ tests/main.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
 
     char *wm_client_machine = NULL;
     char *net_wm_name = NULL;
+    char *net_wm_visible_name = NULL;
+    char *net_wm_icon_name = NULL;
 
     ewmh_uint_list_t net_client_list;
     uint32_t net_number_of_desktops = 0;
@@ -45,13 +47,19 @@ int main(int argc, char *argv[])
     /* Get window properties */
     wm_client_machine = icccm_wm_client_machine(w);
     net_wm_name = ewmh_net_wm_name(w);
+    net_wm_visible_name = ewmh_net_wm_visible_name(w);
+    net_wm_icon_name = ewmh_net_wm_icon_name(w);
 
-    printf("WM_CLIENT_MACHINE: \"%s\"\n", wm_client_machine);
-    printf("_NET_WM_NAME:      \"%s\"\n", net_wm_name);
+    printf("WM_CLIENT_MACHINE:      \"%s\"\n", wm_client_machine);
+    printf("_NET_WM_NAME:           \"%s\"\n", net_wm_name);
+    printf("_NET_WM_VISIBLE_NAME:   \"%s\"\n", net_wm_visible_name);
+    printf("_NET_WM_ICON_NAME:      \"%s\"\n", net_wm_icon_name);
 
     /* Clean */
     free(wm_client_machine);
     free(net_wm_name);
+    free(net_wm_visible_name);
+    free(net_wm_icon_name);
 
     return 0;
 }
